de0c689f815bb6830d2442bf9a4dfd4ba1c695b8
rkurtianyk <ruslan.kurtjanyk@gmail.com>
2019-03-06 01:46:46 +0200

    add base view of hue editor

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
+const generateHueChooserBackground = function() {
+    let backgroundGradient = "-webkit-linear-gradient(left, ";
+    for (let i = 10; i < 370; i += 10) {
+        backgroundGradient += `hsla(${i}, 100%, 50%, 1),`
+	}
+	backgroundGradient = backgroundGradient.substring(0, backgroundGradient.length - 1);
+    backgroundGradient += ");";
+    return backgroundGradient;
+}
+
 customElements.define('simple-colorpicker', class extends HTMLElement {
 	constructor() {
-		super();
+		super()
 		
 		this.attachShadow({
 			mode: 'open'
-		});
-		
+		})
+
+		const width = this.getAttribute('width') || null
+		const height = this.getAttribute('height') || null
+		const paletteStyle = `width: ${width}px; height: ${height}px`
+		const backgroundHue = generateHueChooserBackground()
+
 		const template = document.createElement('template');
 		template.innerHTML = `
 			<link rel="stylesheet" href="index.css">
-			<div>Test</div
-		`;
+			<div class="color-picker-container" style="${paletteStyle}">
+				<div class="palette-container">Here will be palette</div>
+				<div class="hue-container">
+					<div class="hue-chooser" style="background: ${backgroundHue}">
+					</div>
+				</div>
+			</div>
+		`
 		
 		this.shadowRoot.appendChild(
 			template.content.cloneNode(true)
-		);
+		)
 	}
 });
