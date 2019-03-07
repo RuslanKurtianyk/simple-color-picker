483dd341bc4e4441cc32d26e9ed804c93b66a248
rkurtianyk <ruslan.kurtjanyk@gmail.com>
2019-03-07 23:08:38 +0200

    add slider to hue chooser element

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const generateHueChooserBackground = function() {
-    let backgroundGradient = "-webkit-linear-gradient(left, ";
+function generateHueChooserBackground() {
+    let backgroundGradient = "-webkit-linear-gradient(left, "
     for (let i = 10; i < 370; i += 10) {
         backgroundGradient += `hsla(${i}, 100%, 50%, 1),`
 	}
-	backgroundGradient = backgroundGradient.substring(0, backgroundGradient.length - 1);
-    backgroundGradient += ");";
-    return backgroundGradient;
+	backgroundGradient = backgroundGradient.substring(0, backgroundGradient.length - 1)
+    backgroundGradient += ");"
+    return backgroundGradient
 }
 
 customElements.define('simple-colorpicker', class extends HTMLElement {
@@ -21,13 +21,14 @@ customElements.define('simple-colorpicker', class extends HTMLElement {
 		const paletteStyle = `width: ${width}px; height: ${height}px`
 		const backgroundHue = generateHueChooserBackground()
 
-		const template = document.createElement('template');
+		const template = document.createElement('template')
 		template.innerHTML = `
 			<link rel="stylesheet" href="index.css">
 			<div class="color-picker-container" style="${paletteStyle}">
 				<div class="palette-container">Here will be palette</div>
 				<div class="hue-container">
-					<div class="hue-chooser" style="background: ${backgroundHue}">
+					<div class="hue-chooser" id="hue-slider" style="background: ${backgroundHue}">
+						<span class="hue-handler"></span>
 					</div>
 				</div>
 			</div>
@@ -35,6 +36,45 @@ customElements.define('simple-colorpicker', class extends HTMLElement {
 		
 		this.shadowRoot.appendChild(
 			template.content.cloneNode(true)
-		)
+		)	
+	}
+
+	connectedCallback() {
+		this.initHueSlider()
+	}
+
+	initHueSlider() {
+		const range = this.shadowRoot.getElementById('hue-slider')
+		const dragger = range.children[0]
+		const draggerWidth = 20
+		let	down = false
+		let	rangeWidth
+		let rangeLeft
+
+		dragger.style.width = `${draggerWidth}px`
+		dragger.style.left = `${-draggerWidth}px`
+		dragger.style.marginLeft = `${(draggerWidth / 2)}px`
+
+		range.addEventListener('mousedown', e => {
+			rangeWidth = this.offsetWidth
+			rangeLeft = this.offsetLeft
+			down = true
+			updateDragger(e)
+			return false
+		})
+
+		document.addEventListener('mousemove', e => {
+			updateDragger(e)
+		})
+
+		document.addEventListener('mouseup', () => {
+			down = false
+		})
+
+		function updateDragger(e) {
+			if (down && e.pageX >= rangeLeft && e.pageX <= (rangeLeft + rangeWidth)) {		
+				dragger.style.left = `${e.pageX - rangeLeft - draggerWidth}px`
+			}
+		}
 	}
 });
