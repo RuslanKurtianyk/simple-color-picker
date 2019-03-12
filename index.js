7137ea6ada4e2ec05de3601793b05ad1db503af5
rkurtianyk <ruslan.kurtjanyk@gmail.com>
2019-03-13 01:24:30 +0200

    add hue value calculating

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 function generateHueChooserBackground() {
-    let backgroundGradient = "-webkit-linear-gradient(left, "
+    let backgroundGradient = '-webkit-linear-gradient(left, '
     for (let i = 10; i < 370; i += 10) {
         backgroundGradient += `hsla(${i}, 100%, 50%, 1),`
 	}
 	backgroundGradient = backgroundGradient.substring(0, backgroundGradient.length - 1)
-    backgroundGradient += ");"
+    backgroundGradient += ');'
     return backgroundGradient
 }
 
 customElements.define('simple-colorpicker', class extends HTMLElement {
+
+
 	constructor() {
 		super()
 		
 		this.attachShadow({
 			mode: 'open'
 		})
+
+		this.type = "hsla"
+		this.hslValue = {
+			hue: 0,
+			saturation: 100,
+			lightness: 50,
+			alpha: 1
+		}
 
 		const width = this.getAttribute('width') || null
 		const height = this.getAttribute('height') || null
@@ -38,6 +48,14 @@ customElements.define('simple-colorpicker', class extends HTMLElement {
 						<span class="hue-handler"></span>
 					</div>
 				</div>
+				<div class="color-result">
+					<div class="result-hsla">
+						<div class="hsla-value" id="hsla-value">
+							${this.generateHslaString()}
+						</div>
+						<div>HSLA</div>
+					</div>
+				</div>
 			</div>
 		`
 		
@@ -51,9 +69,11 @@ customElements.define('simple-colorpicker', class extends HTMLElement {
 	}
 
 	initHueSlider() {
+		const hslaElement = this.shadowRoot.getElementById('hsla-value')
 		const range = this.shadowRoot.getElementById('hue-slider')
 		const dragger = range.children[0]
 		const draggerWidth = 22
+		let hueValue = this.hslValue.hue
 		let	down = false
 		let	rangeWidth
 		let rangeLeft
@@ -70,18 +90,27 @@ customElements.define('simple-colorpicker', class extends HTMLElement {
 			return false
 		})
 
-		document.addEventListener('mousemove', e => {
+		range.addEventListener('mousemove', e => {
 			updateDragger(e)
 		})
 
-		document.addEventListener('mouseup', () => {
+		range.addEventListener('mouseup', () => {
 			down = false
 		})
 
-		function updateDragger(e) {
-			if (down && e.pageX >= rangeLeft && e.pageX <= (rangeLeft + rangeWidth)) {		
-				dragger.style.left = `${e.pageX - rangeLeft - draggerWidth}px`
+		const updateDragger = (e) => {
+			if (down && e.pageX >= rangeLeft && e.pageX <= (rangeLeft + rangeWidth)) {	
+				const hue = e.pageX - rangeLeft
+				const huePercentage = hue / rangeWidth
+				hueValue = parseInt(parseInt(huePercentage * 100) * 3.6)
+				this.hslValue.hue = hueValue
+				hslaElement.innerHTML = this.generateHslaString()
+				dragger.style.left = `${hue - draggerWidth}px`
 			}
 		}
+	}
+
+	generateHslaString() {
+		return `hsla(${this.hslValue.hue}, ${this.hslValue.saturation}%, ${this.hslValue.lightness}%, ${this.hslValue.alpha} )`
 	}
 });
